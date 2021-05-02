bcc84159a3cb881259fbcac98eb05d8c3940f735
vinay13000 <74045128+vinay13000@users.noreply.github.com>
2021-05-02 17:40:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("kgdcgdgcghdgcgdycydgcgygc")
+var fn ="dddd";
+var ln ="bbbb";
+
+var fln = fn.concat(ln);
+console.log(fln);
+
+var a ="10";
+var b ="50";
+
+var add=a+b;
+console.log(add);
+
+var sub=a-b;
+console.log(sub);
+
+var mul=a*b;
+console.log(mul);
+
+var div=a/b;
+console.log(div);
+
+
+
+
+
